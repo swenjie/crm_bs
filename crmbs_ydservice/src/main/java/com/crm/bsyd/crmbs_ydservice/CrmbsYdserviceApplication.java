235cb1f65867b2f24c0375a5d91d940d614fd944
swenjie <shangwenjie1026@126.com>
2021-11-27 10:30:54 +0800

    init

---
+++ crmbs_ydservice/src/main/java/com/crm/bsyd/crmbs_ydservice/CrmbsYdserviceApplication.java
@@ -0,0 +1,13 @@
+package com.crm.bsyd.crmbs_ydservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmbsYdserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmbsYdserviceApplication.class, args);
+	}
+
+}
---
+++ crmbs_ydservice/src/test/java/com/crm/bsyd/crmbs_ydservice/CrmbsYdserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.bsyd.crmbs_ydservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmbsYdserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
